5e8fb2b5c0c34ae1a3a525bb27fb97a004fba9ab
Vitor Sandrin <sandrin610@gmail.com>
2025-05-10 21:04:09 -0300

    feat(protocolo-4ever): construcao do site da protocolo4ever

---
+++ assets/scripts/accordion.js
@@ -0,0 +1,18 @@
+document.addEventListener('DOMContentLoaded', function () {
+    const accordionButtons = document.querySelectorAll('.accordion-button');
+
+    accordionButtons.forEach(button => {
+        button.addEventListener('click', function () {
+            const content = this.nextElementSibling;
+            const chevron = this.querySelector('.accordion-chevron');
+
+            if (content.classList.contains('hidden')) {
+                content.classList.remove('hidden');
+                chevron.classList.add('rotate-180');
+            } else {
+                content.classList.add('hidden');
+                chevron.classList.remove('rotate-180');
+            }
+        });
+    });
+});
